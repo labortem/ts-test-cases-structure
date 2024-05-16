ea9f8a24e3afdcaff6687f0f83d861d22714da06
Lucas Bastian <49524093+Ratibus11@users.noreply.github.com>
2024-05-17 00:05:07 +0200

    Webpack config

--- config/paths.ts
+++ config/paths.ts
@@ -2,11 +2,13 @@ import { resolve } from "path";
 
 export const root = process.cwd();
 
-export const inAppFolder = resolve(root, "src");
-export const entryFile = resolve(inAppFolder, "main.ts");
+export const srcFolder = resolve(root, "src");
+export const entryFile = resolve(srcFolder, "main.ts");
 
 export const outFolder = resolve(root, "build");
 export const outAppFile = resolve(outFolder, "app.js");
-export const outTypesFile = resolve(outFolder, "app.d.ts");
+export const outTypesFolder = resolve(outFolder, "types");
+
+export const configFolder = resolve(root, "config");
 
 export const tsconfig = resolve(root, "tsconfig.json");
--- config/webpack/webpack.prod.ts
+++ config/webpack/webpack.prod.ts
@@ -1,31 +1,46 @@
 import { Configuration } from "webpack";
 
 import * as paths from "../paths";
+
 import { basename } from "path";
 
-export default <Configuration>{
-	entry: paths.entryFile,
-	output: {
-		path: paths.outFolder,
-		filename: basename(paths.outAppFile),
-		clean: true,
-	},
-	mode: "production",
-	optimization: {
-		minimize: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				loader: "ts-loader",
-			},
-		],
-	},
-	resolve: {
-		extensions: [
-			".ts",
-		],
-	},
-	plugins: [],
+module.exports = (env: any, argv: any) => {
+	const isProduction = argv?.mode === "production" ?? false;
+
+	return <Configuration>{
+		entry: paths.entryFile,
+		output: {
+			path: paths.outFolder,
+			filename: basename(paths.outAppFile),
+			clean: true,
+		},
+		mode: isProduction ? "production" : "development",
+		devtool: isProduction ? false : "inline-source-map",
+		optimization: {
+			minimize: isProduction,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					loader: "ts-loader",
+					exclude: [
+						paths.configFolder,
+					],
+					options: {
+						compilerOptions: {
+							declaration: true,
+							declarationDir: paths.outTypesFolder,
+						},
+						onlyCompileBundledFiles: true,
+					},
+				},
+			],
+		},
+		resolve: {
+			extensions: [
+				".ts",
+			],
+		},
+	};
 };
