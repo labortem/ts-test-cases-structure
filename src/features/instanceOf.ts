6fc58be6ddb018fa1146b25f2008609dfde027be
Lucas Bastian <49524093+ratibus11@users.noreply.github.com>
2024-05-22 18:06:03 +0200

    Test job

--- src/features/instanceOf.ts
+++ src/features/instanceOf.ts
@@ -8,6 +8,5 @@ import { Class } from "utils/signatures";
  * @returns Nothing.
  * @remarks That function MUST NOT be used at runtime, as it is only used for type-checking purposes in the test suites.
  */
-export function instanceOf<C extends Class>(c: C): InstanceType<C> {
-	return {} as any;
+export function instanceOf<C extends Class>(c: C): InstanceType<C> {return {} as any;
 }
