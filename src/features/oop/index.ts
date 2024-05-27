43a3a45342e7220cb8d2a8722e77d8b3156c27d3
Lucas Bastian <49524093+Ratibus11@users.noreply.github.com>
2024-05-27 20:29:02 +0200

    Move out oop test cases from TestCaseOf

--- src/features/oop/index.ts
+++ src/features/oop/index.ts
@@ -1,3 +1,6 @@
-export * as TestCaseOf from "./TestCaseOf";
-
+export { TestCaseOfClassGetter } from "./TestCaseOfClassGetter";
+export { TestCaseOfClassMethod } from "./TestCaseOfClassMethod";
+export { TestCaseOfStaticClassGetter } from "./TestCaseOfStaticClassGetter";
+export { TestCaseOfStaticClassMethod } from "./TestCaseOfStaticClassMethod";
+export { TestCaseOfClassConstructor } from "./TestCaseOfClassConstructor";
 export { instanceOf } from "./instanceOf";
