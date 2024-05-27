6bbae9c2662878e35551cd5f977cadd11897ffbc
Lucas Bastian <49524093+Ratibus11@users.noreply.github.com>
2024-05-27 21:05:05 +0200

    Getting must return a value

--- src/utils/structure/has/HasStaticClassGetterReturn.ts
+++
@@ -1,9 +0,0 @@
-import { Getter } from "utils/signatures";
-
-/**
- * Check if the given class static getter does return a value.
- * @internal
- * @typeParam G - Static getter to check.
- * @remarks The returned type is `true` if the static getter does return a value, `false` otherwise.
- */
-export type HasStaticClassGetterReturn<G extends Getter> = G extends void ? false : true;
--- src/utils/structure/isLike/index.ts
+++ src/utils/structure/isLike/index.ts
@@ -1,5 +1,4 @@
 export { IsClassConstructorLike } from "./IsClassConstructorLike";
 export { IsClassGetterLike } from "./IsClassGetterLike";
 export { IsClassMethodLike } from "./IsClassMethodLike";
-export { IsStaticClassGetterLike } from "./IsStaticClassGetterLike";
 export { IsStaticClassMethodLike } from "./IsStaticClassMethodLike";
